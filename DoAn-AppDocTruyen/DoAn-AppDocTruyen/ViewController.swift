a21275029a65815ddf07743b7dea2168b4f1f8f2
Trung Nghiem <nghiem.duc.trung@gmail.com>
2020-02-21 18:53:50 +0700

    push project

---
+++ DoAn-AppDocTruyen/DoAn-AppDocTruyen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoAn-AppDocTruyen
+//
+//  Created by Trung on 2/21/20.
+//  Copyright © 2020 Trung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
